c0ebbf4102022c20aee6792170518870c1cac242
Nat-Nigro <148778618+Nat-Nigro@users.noreply.github.com>
2024-02-06 15:17:05 +0100

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,16 @@
+--SELECT* FROM PRODUCTS
+
+--SELECT PRODUCTNAME FROM PRODUCTS WHERE UnitsInStock >= 40
+
+--SELECT * FROM Employees WHERE CITY = 'London'
+
+--SELECT * FROM Orders ORDER BY Freight DESC
+
+--SELECT * FROM Orders WHERE Freight > 90 AND Freight < 200
+
+--SELECT * FROM Products WHERE CategoryID = 1
+
+--SELECT * FROM [Order Details] WHERE Discount > 0
+
+--SELECT* FROM Orders WHERE CustomerID='BOTTM' AND Freight > 50
+
